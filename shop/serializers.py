401ecb88e0393f5ff7bc44f587003a6dca466327
Edwin King'ori <eddmunyiri@gmail.com>
2024-12-06 12:27:10 +0300

    serializing relational fields

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Collection
 from decimal import Decimal
 
 
@@ -10,6 +10,10 @@ class ProductSerializer(serializers.Serializer):
         max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
+    collection = serializers.PrimaryKeyRelatedField(
+        queryset=Collection.objects.all()
+    )
+
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
