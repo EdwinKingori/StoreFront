d4e8514145d850c3c869d7f5cfc09cef9cd2a1e1
Edwin King'ori <eddmunyiri@gmail.com>
2024-12-06 12:42:21 +0300

    nesting the collection serializer products

--- shop/serializers.py
+++ shop/serializers.py
@@ -3,6 +3,11 @@ from .models import Product, Collection
 from decimal import Decimal
 
 
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
@@ -10,7 +15,7 @@ class ProductSerializer(serializers.Serializer):
         max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
