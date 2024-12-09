ec6f72ce4dc53b66aeaf51235d6369f2db591df7
Edwin King'ori <eddmunyiri@gmail.com>
2024-12-09 16:37:14 +0300

    replacing paths with routers in the urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 from . import views
+from pprint import pprint
 
 router = SimpleRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 router.urls
+pprint(router.urls)
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product_list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-    path('collections/', views.CollectionList.as_view(), name='collection-list'),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail')
-]
+urlpatterns = router.urls
 
-
+# urlpatterns =[
+#     # path('products/', views.ProductList.as_view(), name='product_list'),
+#     # path('products/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
+#     # path('collections/', views.CollectionList.as_view(), name='collection-list'),
+#     # path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail')
+# ]
